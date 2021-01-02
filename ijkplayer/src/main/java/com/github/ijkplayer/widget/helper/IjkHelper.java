a0c3966e4eb31ec32dba316b99371450c311e03f
zhongrui <zhongrui@021.com>
2021-01-02 17:54:01 +0800

    file

---
+++ ijkplayer/src/main/java/com/github/ijkplayer/widget/helper/IjkHelper.java
@@ -0,0 +1,4 @@
+package com.github.ijkplayer.widget.helper;
+
+public class IjkHelper {
+}
--- ijkplayer/src/main/java/com/github/ijkplayer/widget/helper/MediaPlayerService.java
+++ ijkplayer/src/main/java/com/github/ijkplayer/widget/helper/MediaPlayerService.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.example.ijkplayerproject.ijk.widget;
+package com.github.ijkplayer.widget.helper;
 
 import android.app.Service;
 import android.content.Context;
